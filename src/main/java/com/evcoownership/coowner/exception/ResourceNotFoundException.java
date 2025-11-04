4943ebcb61d52cf2b79a6ef37ab9c3c8369a1a32
hoanglam14111 <hoanglam270105@gmail.com>
2025-11-05 00:26:34 +0700

    rename

--- src/main/java/com/evcoownership/coowner/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.evcoownership.coowner.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
