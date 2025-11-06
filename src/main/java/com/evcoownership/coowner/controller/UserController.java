79cf899d7e32183def279cf383ccb43f42592b27
BaTrungTin <batrungtin0705@gmail.com>
2025-11-07 00:44:44 +0700

    Reapply "Feature/trinh"
    This reverts commit 4dde60d4181708ff4114b368c5679930ee7dc8e1.

--- src/main/java/com/evcoownership/coowner/controller/UserController.java
+++ src/main/java/com/evcoownership/coowner/controller/UserController.java
@@ -8,9 +8,14 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/api/users")
+@Tag(name = "user-controller", description = "User management endpoints")
 public class UserController {
 
     private final UserService userService;
@@ -20,8 +25,8 @@ public class UserController {
     }
 
     @GetMapping
-    public List<UserDto> list() {
-        return userService.listUsers();
+    public Page<UserDto> list(@PageableDefault(size = 10) Pageable pageable) {
+        return userService.listUsers(pageable);
     }
 
     @PostMapping("/register")
